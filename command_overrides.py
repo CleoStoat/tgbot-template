1ffeb4c27818cdeec43789a4e21a3f2ad00d0b10
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2021-11-27 19:55:23 -0300

    Added command overrides

---
+++ command_overrides.py
@@ -0,0 +1,11 @@
+from handlers.handler_model import CommandModel
+
+
+OVERRIDES = {
+    "command_name": {
+        "priority": "",
+        "name": "new_command_name",
+        "description": "New command description",
+        "hidden": False,
+    }
+}
