031c1d6724327d7df0786c56c8cb1fe03fbfcf90
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2021-11-27 20:44:58 -0300

    Removed hidden from commands

--- handlers/command_list.py
+++ handlers/command_list.py
@@ -6,18 +6,15 @@ COMMANDS = [
         priority=200,
         name="help",
         description="Get help",
-        hidden=True,
     ),
     CommandModel(
         priority=200,
         name="start",
         description="Start the bot",
-        hidden=True,
     ),
     CommandModel(
         priority=200,
         name="settings",
         description="Settings for this bot",
-        hidden=True,
     ),
 ]
