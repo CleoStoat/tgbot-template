71c94da23c90ac77ac3a2678205b1c705b7c42d0
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2021-11-27 19:37:48 -0300

    Added command example in command_list

---
+++ handlers/command_list.py
@@ -0,0 +1,11 @@
+from handlers.handler_model import CommandModel
+
+COMMANDS = [
+    CommandModel(
+        priority=200,
+        callback=lambda x: None,
+        name="command_name",
+        description="The description of the command",
+        hidden=True,
+    ),
+]
