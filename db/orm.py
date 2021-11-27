3e6633b70c20d172f500e34e60d2c15ff5c8b51b
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2021-11-27 18:40:01 -0300

    Added orm mappings

---
+++ db/orm.py
@@ -0,0 +1,46 @@
+from sqlalchemy import (
+    Table,
+    MetaData,
+    Column,
+    Integer,
+    String,
+    Numeric,
+    DateTime,
+    Boolean,
+)
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import mapper
+from sqlalchemy.sql.functions import user
+
+from db.model import UserInfo, UserCode
+import config
+
+metadata = MetaData()
+
+user_info = Table(
+    "user_infos",
+    metadata,
+    Column("user_id", Integer, primary_key=True, autoincrement=False),
+    Column("first_name", String),
+    Column("last_name", String),
+    Column("username", String),
+    Column("money", Numeric),
+    Column("last_updated", DateTime),
+    Column("active", Boolean),
+)
+
+user_code = Table(
+    "user_codes",
+    metadata,
+    Column("user_id", Integer, primary_key=True, autoincrement=False),
+    Column("code", String, primary_key=True, autoincrement=False),
+)
+
+def start_mappers():
+    mapper(UserInfo, user_info)
+    mapper(UserCode, user_code)
+
+
+def create_tables():
+    engine = create_engine(config.get_sqlite_uri())
+    metadata.create_all(engine)
